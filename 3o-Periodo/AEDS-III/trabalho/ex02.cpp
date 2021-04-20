65709ed49ec6502c0973eaddeda9d30889a54ebd
Italo <geraldo.henrique@touchcomp.com.br>
2021-04-20 12:45:53 -0300

    finishing ex02

--- 3o-Periodo/AEDS-III/trabalho/ex02.cpp
+++ 3o-Periodo/AEDS-III/trabalho/ex02.cpp
@@ -2,28 +2,58 @@
 #include<stdio.h>
 #include<vector>
 #include<string>
+#include<iostream>
 
 using namespace std;
 
-vector<string> addList(vector<string> list, string name){
-    list.push_back(name);
+vector<string> addToList(vector<string> vect, string item){
+    vect.push_back(item);
+    return vect;
 }
 
-void printVec(vector<string> list){
-    for(int idx = 0; idx<list.size(); idx++){
-        printf(" %s \n", list[idx].c_str());
+void printList(vector<string> vect){
+    for(int idx = 0; idx<vect.size(); idx++){
+        printf(" %s ", vect[idx].c_str());
     }
+}
+
+bool existThisItem(vector<string> vect, string item){
+    for(int idx = 0; idx<vect.size(); idx++){
+        if(vect[idx] == item) return true;
+    }
+    return false;
 }
 
 
 int main(void){
-    vector<string> produtos;
+    vector<string> product;
+    string nameItem;
+    int numLists = 2;
 
-    produtos.push_back("um");
-    produtos.push_back("dois");
-    produtos.push_back("tres");
+    for(int i=0; i<numLists; i++){    
+        product.clear();
 
-    printVec(produtos);
+        printf("\t----------< Lista %d >----------\n", i+1);
+
+        do{
+            printf("Digite o nome do produto (Para sair digite \"quit\" )\nR.: ");
+            cin >> nameItem;
+
+            if(nameItem == "quit") continue;
+
+            if(!existThisItem(product, nameItem)){
+                product = addToList(product, nameItem);
+            };
+            
+            printf("\n");
+
+        } while(nameItem != "quit" );
+
+        printf("Lista de produtos: ");
+        printList(product);
+
+        printf("\n\n");
+    }
 
     system("pause");
     return 0;
