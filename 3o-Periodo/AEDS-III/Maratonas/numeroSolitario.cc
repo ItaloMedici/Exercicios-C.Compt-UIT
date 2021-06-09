ba625a476a9c19b5eb5b2daf03f04779fab5b643
ItaloMedici <italomedice969@gmail.com>
2021-06-09 13:44:59 -0300

    alterações nas pastas e adição dos exercicios de maratona

---
+++ 3o-Periodo/AEDS-III/Maratonas/numeroSolitario.cc
@@ -0,0 +1,43 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+bool isNumExists(int *vetor, int num, int size) {
+    for(int i=0 ; i<size; i++) {
+        if(num == vetor[i]) return true;
+    } 
+    return false;
+}
+
+void getOdds(int *origin, int *odds, int size) {
+    for(int i=0; i<size; i++) {
+        int count = 0;
+
+        for(int j=0; j<size; j++) {
+            origin[i] == origin[j] ? count++ : 0 ;
+        }
+ 
+        int idx = 0;
+        if(count%2 != 0 && !isNumExists(odds, origin[i], size)) {
+            printf(" %d ", origin[i]);
+        }
+    }
+}
+
+int main () {
+    int qntd = -1;
+
+    do {
+        scanf("%d", &qntd);
+        int nums[qntd];
+
+        for(int i=0; i<qntd; i++) {
+            scanf("%d", &nums[i]);
+        }
+
+        int lonely[qntd];
+        getOdds(nums, lonely, qntd);
+    } while (qntd > 0);
+
+    return 0;
+}
+
