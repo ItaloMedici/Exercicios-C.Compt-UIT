c9bbbf548612a043363fa8e2776fe77a7839138b
ItaloMedici <italomedice969@gmail.com>
2021-08-13 22:31:53 -0300

    Adding 4 periodo

---
+++ 4o-Periodo/POO-I/aula1/Caneta.java
@@ -0,0 +1,19 @@
+package aula1;
+
+public class Caneta {
+    Double tamanho;
+    Boolean temTinta;
+    String cor;
+
+    public boolean temTinta() {
+        return temTinta;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCort() {
+        return cor;
+    }
+}
