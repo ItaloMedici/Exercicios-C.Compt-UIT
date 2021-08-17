0457dd2a553d571f656b9ca3400950c35f391603
ItaloMedici <italomedice969@gmail.com>
2021-08-17 09:35:43 -0300

    create caneta and main class, update gitignore

--- 4o-Periodo/POO-I/aula1/Caneta.java
+++ 4o-Periodo/POO-I/aula1/Caneta.java
@@ -1,19 +1,69 @@
 package aula1;
 
 public class Caneta {
-    Double tamanho;
-    Boolean temTinta;
-    String cor;
+    private String marca;
+    private Boolean temTinta;
+    private String cor;
+    private Integer uso;
 
     public boolean temTinta() {
         return temTinta;
+    }
+
+    public void porTinta() {
+        if(!temTinta()){
+            this.temTinta = true;
+            this.zerarUso();
+        }
+    }
+
+    public void usar(){
+        uso++;
+        if (uso == 5) {
+            temTinta = false;
+        }
+    }
+
+    public void zerarUso() {
+        uso = 0;
+    }
+
+    public String getCor() {
+        return cor;
     }
 
     public void setCor(String cor) {
         this.cor = cor;
     }
 
-    public String getCort() {
-        return cor;
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void printCaneta(){
+        System.out.println(
+            ""
+            + "\nMarca: " + marca
+            + "\nCor: " + cor
+            + "\nTem tinta: " + (temTinta() ? "Sim" : "Não")
+            + "\nUso: " + uso
+        );
+    }
+
+    public Caneta() {
+        this.cor = "";
+        this.marca = "";
+        this.temTinta = true;
+        this.uso = 0;
+    }
+
+    public Caneta(String marca, String cor) {
+        this();
+        this.marca = marca;
+        this.cor = cor;
     }
 }
