a86d22d2e21b347861a32db19f8dab6df3fa769b
ItaloMedici <italomedice969@gmail.com>
2021-08-24 14:10:23 -0300

    updates and create strToDate method on utls

--- 4o-Periodo/POO-I/utils/FormatterUtil.java
+++ 4o-Periodo/POO-I/utils/FormatterUtil.java
@@ -1,8 +1,10 @@
 package utils;
 
 import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 /**
  *
@@ -12,24 +14,32 @@ import java.util.Date;
 public class FormatterUtil {
     private static final String DUAS_CASAS = "#,##0.00";
     private static final String CURRECY = "¤#,##0.00;¤-#,##0.00";
-    private static final String DD_MM_YYYY = "dd/MM/yyyy";;
+    private static final String DD_MM_YYYY = "dd/MM/yyyy";
 
+    // Formatadores de numero
     public static String formatarNumeroDuasCasas(Double numero) {
-        if(numero == null) return "";
+        if (numero == null)
+            return "";
 
         DecimalFormat d = new DecimalFormat(DUAS_CASAS);
         return d.format(numero);
     }
 
     public static String formatarEmReais(Double numero) {
-        if(numero == null) return "";
+        if (numero == null)
+            return "";
 
         DecimalFormat d = new DecimalFormat(CURRECY);
         return d.format(numero);
     }
 
+    public static String comPorcentagem(Double num) {
+        return formatarNumeroDuasCasas(num) + "%";
+    }
+
+    // Datas
     public static String dataEmStr(Date data) {
-        if(data == null){
+        if (data == null) {
             return "";
         }
 
@@ -37,19 +47,32 @@ public class FormatterUtil {
         return sd.format(data);
     }
 
-    public static String comPorcentagem(Double num){
-        return formatarNumeroDuasCasas(num) + "%";
+    public static Date strEmData(String data) {
+        try {
+            if (data != null) {
+                SimpleDateFormat d = new SimpleDateFormat(DD_MM_YYYY);
+                Date tempDate = d.parse(data);
+                if (!d.format(tempDate).equals(data)) {
+                    throw new ParseException("", 5);
+                } else {
+                    return tempDate;
+                }
+            } 
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
     }
-
+    
+    // Formatadores de String
     public static String formatarTelefone(String telefone) {
         String tel2 = "";
 
-        if(telefone.length()==11) {
-            tel2 = "(" + telefone.substring( 0, 2 ) + ") "
-            + telefone.substring( 2, 7 ) + "-"
-            + telefone.substring( 7, 11 );
+        if (telefone.length() == 11) {
+            tel2 = "(" + telefone.substring(0, 2) + ") " + telefone.substring(2, 7) + "-" + telefone.substring(7, 11);
         }
-        
+
         return tel2;
     }
 }
