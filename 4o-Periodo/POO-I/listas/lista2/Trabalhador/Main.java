8c44603d9451a0d9cb1690bd1bb3af3319e4d4b0
ItaloMedici <italomedice969@gmail.com>
2021-09-12 22:18:05 -0300

    add lista 2 e exercicio 1

---
+++ 4o-Periodo/POO-I/listas/lista2/Trabalhador/Main.java
@@ -0,0 +1,32 @@
+package lista2.Trabalhador;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    List<Trabalhador> trabalhadores = new ArrayList<>();
+    Scanner s = new Scanner(System.in);
+  
+    for(int i=0; i<3; i++) {
+      System.out.println("\tTrabalhador " + (i+1));
+  
+      System.out.println("Nome");
+      String nome = s.nextLine();
+  
+      System.out.println("Matricula");
+      String matricula = s.nextLine();
+  
+      System.out.println("Salario");
+      Double salario = s.nextDouble();
+  
+      trabalhadores.add(new Trabalhador(nome, matricula, salario));
+    }
+
+    trabalhadores.forEach(t -> System.out.println(t.toString()));
+
+    s.close();
+  }
+}
---
+++ 4o-Periodo/POO-I/listas/lista2/Trabalhador/Trabalhador.java
@@ -0,0 +1,63 @@
+package lista2.Trabalhador;
+
+import java.text.DecimalFormat;
+
+/**
+ * Crie a classe Trabalhador com os atributos matrícula, nome e salário. Crie
+ * uma outra classe com o método main e peça ao usuário para cadastrar 3
+ * trabalhadores. Lembre-se que ninguém pode ganhar menos que um salário mínimo.
+ */
+
+public class Trabalhador {
+  private String nome;
+  private String matricula;
+  private Double salario;
+
+  private final Double salarioMinimo = 1100d;
+
+  public String getNome() {
+    return nome;
+  }
+
+  private void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getMatricula() {
+    return matricula;
+  }
+
+  public void setMatricula(String matricula) {
+    this.matricula = matricula;
+  }
+
+  public Double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(Double salario) {
+    if (salario < salarioMinimo) {
+      this.salario = salarioMinimo;
+      return;
+    }
+    this.salario = salario;
+  }
+
+  public String getSalarioFormatado() {
+    return new DecimalFormat("¤#,##0.00;¤-#,##0.00").format(salario);
+  }
+
+  public Trabalhador(String nome, String matricula, Double salario) {
+    setNome(nome);
+    this.matricula = matricula;
+    setSalario(salario);
+  }
+
+  @Override
+  public String toString() {
+    return 
+      "\nNome: " + nome 
+      + "\nMatricula: " + matricula 
+      + "\nSalario: " + getSalarioFormatado(); 
+  }
+}
