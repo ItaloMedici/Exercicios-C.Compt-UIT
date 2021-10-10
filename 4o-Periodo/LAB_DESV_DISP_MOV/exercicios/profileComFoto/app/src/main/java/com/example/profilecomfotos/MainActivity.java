3442934dd7de2255f1bc74cf47de14788a0d3b2f
ItaloMedici <italomedice969@gmail.com>
2021-10-10 00:03:53 -0300

    Exercicio de profile com foto

---
+++ 4o-Periodo/LAB_DESV_DISP_MOV/exercicios/profileComFoto/app/src/main/java/com/example/profilecomfotos/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.profilecomfotos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageProfile;
+    private Button buttonCamera;
+    static final int CAPTURE_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageProfile = (ImageView) findViewById(R.id.imageProfile);
+        buttonCamera = (Button) findViewById(R.id.buttonCamera);
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, CAPTURE_IMAGE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageProfile.setImageBitmap(imageBitmap);
+        }
+    }
+}
