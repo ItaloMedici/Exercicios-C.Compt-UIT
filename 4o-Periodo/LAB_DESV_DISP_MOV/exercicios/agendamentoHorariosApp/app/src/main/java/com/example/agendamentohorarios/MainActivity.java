fa281c1eb3a14d588cf6f1f3df4cbcff8d27a058
ItaloMedici <italomedice969@gmail.com>
2021-10-22 16:48:09 -0300

    design telas app

---
+++ 4o-Periodo/LAB_DESV_DISP_MOV/exercicios/agendamentoHorariosApp/app/src/main/java/com/example/agendamentohorarios/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.agendamentohorarios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        getSupportActionBar().hide();
+    }
+}
